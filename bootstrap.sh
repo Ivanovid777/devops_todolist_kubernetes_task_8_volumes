e1d37c72d85e770017c300e4b40002bc237f520a
ТвоÑIvan <ivanqqqe@gmail.com>
2026-05-17 15:36:34 +0200

    servise has changed on nodeport

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ kind create cluster --config cluster.yml &&
 cd .infrastructure/
 kubectl apply -f namespace.yml &&
 
-kubectl apply -f clusterIp.yml -n todoapp &&
+kubectl apply -f nodeport.yml -n todoapp &&
 
 kubectl apply -f pv.yml -n todoapp &&
 kubectl apply -f pvc.yml -n todoapp &&
